0a63ff3a9ef335b79d768683b55ba3c06e4d3c58
Meriem Afaf Haddou <jm_haddou@esi.dz>
2025-09-09 15:25:49 +0100

    translating main page to english and apply pep8 to it

---
+++ relio.py
@@ -0,0 +1,67 @@
+import streamlit as st
+
+
+st.logo("images/logo.png")
+st.set_page_config(
+    page_title="Relio - Accueil",
+    page_icon="images/icon.png",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+st.write("""
+# 🏠 RELIO - Home
+         """)
+st.markdown(""" """)
+
+
+cd_container = st.container(border=True)
+with cd_container:
+    st.write(""" ### :bulb: RELIO API""")
+    col1, col2 = st.columns([2, 1], gap="large")
+    col1.write("""
+      RELIO API is a programming interface that allows you to detect concept
+      drift caused by distribution changes in real-time data streams, using
+      optimal transport to measure the minimum cost of moving from one data
+      distribution to another.
+      """)
+    col2.image("images/logo.png", width=300)
+st.write("""
+### ▶️ Documentation
+""")
+st.page_link("pages/documentation.py", label="Documentation", icon="📑")
+st.divider()
+st.write("""
+### ▶️ Data generation
+""")
+st.page_link("pages/data_generation.py",
+             label="Data generation", icon="🔻")
+
+st.divider()
+st.write("""
+### ▶️ Simulation of different types of drifts
+""")
+st.page_link("pages/simulation_iris_gradual.py",
+             label=" Simulation : Iris Gradual", icon="🔻")
+st.page_link("pages/simulation_iris_incremental.py",
+             label=" Simulation : Iris Incremental", icon="🔻")
+st.page_link("pages/simulation_iris_sudden.py",
+             label=" Simulation : Iris Sudden", icon="🔻")
+st.page_link("pages/simulation_iris_recurrent.py",
+             label=" Simulation : Iris Recurrent", icon="🔻")
+st.divider()
+
+st.write("""
+### ▶️ Tests on datasets
+""")
+st.page_link("pages/synthetic_data.py",
+             label=" Tests : Synthetic Datasets", icon="🔻")
+st.page_link("pages/real_data.py",
+             label=" Tests : Real Datasets", icon="🔻")
+st.page_link("pages/app_social_modia.py",
+             label=" Application : Social Media", icon="🔻")
+
+st.divider()
+st.write("""
+### ▶️ Comparison with other solutions
+""")
+st.page_link("pages/comparaison.py", label=" Comparaison", icon="🔻")
